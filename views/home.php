7270b50bd22fdfcf4b3a09858849e4e3bb60186c
Ekene <83151271+Yeahx1000@users.noreply.github.com>
2026-01-30 14:34:42 -0500

    complete view

--- views/home.php
+++ views/home.php
@@ -1,1 +1,11 @@
 <!-- this is the home view for the project -->
+<!DOCTYPE html>
+<html>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simple PHP</title>
+    <body>
+        <h1>Simple PHP</h1>
+        <p>Hello World</p>
+    </body>
+</html>
